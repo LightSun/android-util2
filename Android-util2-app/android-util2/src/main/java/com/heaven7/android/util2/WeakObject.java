c884fc1ebdeb4460be7933687e611f209ac47f06
LightSun <532278976@qq.com>
2017-07-12 18:06:57 +0800

    add weak object

---
+++ Android-util2-app/android-util2/src/main/java/com/heaven7/android/util2/WeakObject.java
@@ -0,0 +1,78 @@
+package com.heaven7.android.util2;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Build;
+import android.util.Log;
+import android.view.View;
+
+import com.heaven7.core.util.Logger;
+import com.heaven7.java.base.util.Throwables;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * this class help we handle weak reference object. such as: Activity, fragment, view.
+ * Created by heaven7 on 2017/7/12 0012.
+ * @since 1.0.1
+ */
+public class WeakObject<T> {
+
+    private static final String TAG = "WeakObject";
+    private final WeakReference<T> mWeakRef;
+
+    public WeakObject(T obj){
+        Throwables.checkNull(obj);
+        mWeakRef = new WeakReference<T>(obj);
+    }
+
+    /**
+     * get the raw object.
+     * @return the raw object. may be null if is recycled or unexpected state occurs.
+     */
+    public T get(){
+        T t = mWeakRef.get();
+        if(t == null){
+            return null;
+        }
+        final String name = t.getClass().getName();
+        if(t instanceof Activity){
+            final Activity ac = (Activity) t;
+            if(ac.isFinishing()){
+                Logger.w(TAG,"get","the activity(" + name + ") is finishing.");
+                return null;
+            }
+            if (Build.VERSION.SDK_INT >= 17 && ac.isDestroyed()) {
+                Log.w("WeakActivityOwner","getActivity(): memory leaked ? activity = "
+                        + name);
+                return null;
+            }
+            return t;
+        }
+        if(t instanceof android.support.v4.app.Fragment){
+            final android.support.v4.app.Fragment frag = (android.support.v4.app.Fragment) t;
+            if(frag.isDetached() || frag.isRemoving()){
+                Logger.w(TAG,"get","fragment is detached or removing. fragment = " + name);
+                return null;
+            }
+            return t;
+        }
+        if( t instanceof Fragment){
+            final Fragment frag = (Fragment) t;
+            if(frag.isDetached() || frag.isRemoving()){
+                Logger.w(TAG,"get","fragment is detached or removing. fragment = " + name);
+                return null;
+            }
+            return t;
+        }
+        if(t instanceof View){
+            if(Build.VERSION.SDK_INT >= 19 && !((View) t).isAttachedToWindow()){
+                Logger.w(TAG,"get","view is detached. memory leaked?  view = " + name);
+                return null;
+            }
+            return t;
+        }
+        return t;
+    }
+
+}
