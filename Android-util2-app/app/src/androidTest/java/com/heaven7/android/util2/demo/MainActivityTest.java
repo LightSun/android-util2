a18ae169e09adf1d079c332c25485de644ca715a
LightSun <532278976@qq.com>
2017-07-14 18:29:36 +0800

    no

---
+++ Android-util2-app/app/src/androidTest/java/com/heaven7/android/util2/demo/MainActivityTest.java
@@ -0,0 +1,13 @@
+package com.heaven7.android.util2.demo;
+
+/*
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class MainActivityTest {
+
+  @Test
+  public void titleIsCorrect() throws Exception {
+    Activity activity = Robolectric.setupActivity(MainActivity.class);
+    assertTrue(activity.getTitle().toString().equals("Deckard"));
+  }
+}*/
---
+++ Android-util2-app/app/src/main/java/com/heaven7/android/util2/demo/MyApplication.java
@@ -0,0 +1,10 @@
+package com.heaven7.android.util2.demo;
+
+import android.app.Application;
+
+/**
+ * Created by heaven7 on 2017/7/14 0014.
+ */
+
+public class MyApplication extends Application {
+}
