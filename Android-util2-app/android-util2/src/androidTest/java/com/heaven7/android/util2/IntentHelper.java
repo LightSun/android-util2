88afa030c3c10b43d6ca8653bddf729dd6de588c
LightSun <532278976@qq.com>
2017-07-14 18:51:45 +0800

    no

--- Android-util2-app/android-util2/src/androidTest/java/com/heaven7/android/util2/IntentHelper.java
+++ Android-util2-app/android-util2/src/androidTest/java/com/heaven7/android/util2/IntentHelper.java
@@ -14,6 +14,7 @@ import java.util.List;
 /**
  * stack over flow of android.
  * https://stackoverflow.com/documentation/android/103/intent#t=201707140757431233948
+ * 所有 android 的文档： https://stackoverflow.com/documentation/android/topics?page=5&tab=popular
  * Created by heaven7 on 2017/7/14 0014.
  */
 
